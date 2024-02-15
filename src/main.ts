a9a7b715fbe47fe3486a0a9a5f1b1485fe03b052
Vladyslav Buhai <vladbugaj30@gmail.com>
2024-02-15 23:09:11 +0200

    set up swagger and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/App';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { JWT_BEARER_SWAGGER_AUTH_NAME } from './shared/utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger(AppModule.name);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('News Blog project API')
+    .setDescription('The News Blog project API endpoints documentation.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      JWT_BEARER_SWAGGER_AUTH_NAME,
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const port = parseInt(process.env.API_PORT) ?? 3000;
   logger.log(`Server started on port ${port}`);
