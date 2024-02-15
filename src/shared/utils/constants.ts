976477f6caec439bba952b1f7fcc668482edf184
Vladyslav Buhai <vladbugaj30@gmail.com>
2024-02-15 23:13:36 +0200

    add constants for pagination, jwt and sork user keys

---
+++ src/shared/utils/constants.ts
@@ -0,0 +1,13 @@
+export const DEFAULT_PAGINATION_LIMIT = 10;
+export const DEFAULT_PAGINATION_PAGE = 1;
+
+export const JWT_BEARER_SWAGGER_AUTH_NAME = 'JWT-auth';
+
+export const USERS_SORT_KEYS = [
+  'fullname',
+  'email',
+  'role',
+  'createdAt',
+  'updatedAt',
+  'deletedAt',
+];
