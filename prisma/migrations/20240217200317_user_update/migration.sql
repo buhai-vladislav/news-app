2fc5cf1b1d442a32a7e72185c3423fb6f60c08d8
Vladyslav Buhai <vladbugaj30@gmail.com>
2024-02-18 12:55:47 +0200

    add migration to update user model by optional fields

---
+++ prisma/migrations/20240217200317_user_update/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated` on the `Files` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Files` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Files" DROP COLUMN "updated",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "userId" DROP NOT NULL;
