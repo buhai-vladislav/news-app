71973908681142ff1f2f6b5c3c2b946c7d5cb6d0
Vladyslav Buhai <vladbugaj30@gmail.com>
2024-03-25 19:20:58 +0200

    fix: add array converation support

--- fe-cms-app/src/shared/utils/createFormData.ts
+++ fe-cms-app/src/shared/utils/createFormData.ts
@@ -1,7 +1,9 @@
 export const createFormData = (data: { [key: string]: any }) => {
   const formData = new FormData();
   Object.keys(data).forEach((key) => {
-    formData.append(key, data[key]);
+    Array.isArray(data[key])
+      ? formData.append(key, JSON.stringify(data[key]))
+      : formData.append(key, data[key]);
   });
   return formData;
 };
